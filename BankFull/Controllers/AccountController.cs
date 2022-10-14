b8a6a1d97a0f9f628eb5d9bc3f1a2206f1802c97
nabin166 <nabinaryal830@gmail.com>
2022-10-14 15:19:54 +0545

    Validate_Registration

--- BankFull/Controllers/AccountController.cs
+++ BankFull/Controllers/AccountController.cs
@@ -86,11 +86,56 @@ namespace BankFull.Controllers
             return RedirectToAction("Index", "Account");
         }
 
-        public async Task<IActionResult> Register()
+        public  IActionResult Register()
         {
             ViewData["RoleId"] = new SelectList(_context.Roles.Where(x=>x.Role1!="Admin"), "Id", "Role1");
 
+ 
             return View();
         }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address,Email,Phone,Status,RoleId,Password")] User user , string repeatpassword)
+        {
+            if(user.Password == repeatpassword)
+            {
+                if (ModelState.IsValid)
+                {
+
+                    // User usr = new User();
+
+                    if (_context.user.Where(x => x.Email == user.Email).Count() != 1)
+                    {
+
+
+                        string passwordHash = BCrypt.Net.BCrypt.HashPassword(user.Password);
+                        user.Password = passwordHash;
+                        user.Status = false;
+
+
+                        _context.Add(user);
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                    else
+                    {
+                        ViewData["error"] = "Username already exist";
+                        return RedirectToAction("Register", "Account");
+                    }
+
+
+                }
+            }
+
+           
+
+            ViewData["RoleId"] = new SelectList(_context.Roles, "Id", "Id", user.RoleId);
+            return RedirectToAction("Register", "Account");
+        }
+
+
     }
 }
